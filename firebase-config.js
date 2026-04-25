f740e5f5f8d99f827d9ad244be91a175ae29f555
bebebecity <bebebecity.1234@gmail.com>
2026-04-26 00:02:00 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyBDZtgDi6Brun6G_ojfl7YBCj0NWRcBu7g",
-  authDomain: "kakeimo.firebaseapp.com",
-  projectId: "kakeimo",
-  storageBucket: "kakeimo.firebasestorage.app",
-  messagingSenderId: "1074277480943",
-  appId: "1:1074277480943:web:0f1f7dafba1330389e1e1a"
-};
+
